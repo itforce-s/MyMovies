1ad138af5fa74215ec2225c92763eec41a99795c
itforce-s <itforce.mail@gmail.com>
2022-05-06 10:25:33 +0300

    test

--- app/src/main/java/com/shilo/mymovies/data/MovieDatabase.java
+++ app/src/main/java/com/shilo/mymovies/data/MovieDatabase.java
@@ -1,4 +1,8 @@
 package com.shilo.mymovies.data;
 
-public class MovieDatabase {
+import androidx.room.RoomDatabase;
+
+public abstract class MovieDatabase extends RoomDatabase {
+    private MovieDatabase database;
+
 }
